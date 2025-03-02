918bca4f3fcd8ac25bda911ca29b72698564ed11
Vitor Gomes <vitor.ggomes@aec.com.br>
2025-03-01 21:53:58 -0300

    update: .env DataBase Config

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,7 +9,7 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
-const sql = postgres(process.env.DATABASE_URL!, { ssl: 'verify-full' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
